2da80cf68f8a1ea30c0112f56871e7bed85bf860
Mihail <tamsjam@mail.ru>
2023-01-25 17:02:27 +0300

    Домашняя Работа

---
+++ HomeWork_1/HomeWork/Program.cs
@@ -0,0 +1,19 @@
+﻿///Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+double a = 10;
+double b = 20;
+if (a < b)
+{
+    Console.WriteLine($"Max = {b}");
+    Console.WriteLine($"Min = {a}");
+}
+else if (b < a)
+{
+    Console.WriteLine($"Min = {a}");
+    Console.WriteLine($"Max = {b}");
+}
+else
+{
+    Console.WriteLine("Число {b} ровно числу {a}");
+}
+Console.ReadKey();
