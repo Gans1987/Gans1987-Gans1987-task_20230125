5ac683e3104ab8ac146a624688fbe9f9632223d8
Mihail <tamsjam@mail.ru>
2023-01-31 16:36:04 +0300

    seminar_3

---
+++ HomeWork_1/HomeWerk_8/Program.cs
@@ -0,0 +1,14 @@
+﻿///Задача 1: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+int num = 54345;
+int mun = 0;
+int multi = 10000;
+int n = num;
+while (n > 0)
+{
+    mun += n % 10 * multi;
+    n /= 10;
+    multi /= 10;
+}
+Console.WriteLine(mun == num ? "да" : "нет");
+Console.ReadKey();
