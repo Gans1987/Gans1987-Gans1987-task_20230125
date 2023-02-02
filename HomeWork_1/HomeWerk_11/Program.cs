2c7f6356c3ed0868004e5a787b2eaf5742c1f48a
Mihail <tamsjam@mail.ru>
2023-02-02 15:42:51 +0300

    Домашняя работа

---
+++ HomeWork_1/HomeWerk_11/Program.cs
@@ -0,0 +1,13 @@
+﻿///Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. 
+///Реализовать через функции.
+
+static double Power(double a, int b)
+{
+    double res = a;
+    for (int i = 1; i < b; i++)
+        res *= a;
+    return res;
+}
+
+Console.WriteLine(Power(3, 5));
+Console.ReadKey();
