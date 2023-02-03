a64c2ad29cec11987ff923dc5b14f080ea6ce061
Mihail <tamsjam@mail.ru>
2023-02-03 17:51:40 +0300

    Домашняя работа

---
+++ HomeWork_1/HomeWerl_14/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу,
+//которая задает массив из 8 элементов и выводит их на экран.
+
+Random rnd = new Random();
+static void ShowRandomIntArray(int len, int minValue, int maxValue)
+{
+    int[] arr = new int[len];
+    for (int i = 0; i < len; i++)
+    {
+        arr[i] = rnd.Next(minValue, maxValue + 1);
+        Console.Write(arr[i].ToString() + (i + 1 < len ? ", " : ""));
+    }
+}
+
+ShowRandomIntArray(8, 1, 8);
