5f48987fe81b9cbd695c5739b7b00ceba294e414
Mihail <tamsjam@mail.ru>
2023-02-11 19:36:55 +0300

    Домашняя работа

---
+++ HomeWork_1/HomeWerk_21/Program.cs
@@ -0,0 +1,66 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+//и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+
+//1, 7 -> такого числа в массиве нет
+//1, 2 -> 4
+
+Console.Clear();
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int[,] CreateMarix(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(0, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        System.Console.WriteLine();
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write($"{matrix[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int FindIndex(int[,] matrix, int num1, int num2)
+{
+
+    if (num1 < 0 || num2 < 0 || num1 > matrix.GetLength(0)-1 || num2 > matrix.GetLength(1)-1)
+    {
+        System.Console.WriteLine("Вы вышли за пределы массива");
+        return 0;
+    }
+
+
+    return matrix[num1, num2];
+
+}
+int rows = Prompt("Введите количиство строк: ");
+int columns = Prompt("Введите количество столбцов: ");
+int[,] matrix = CreateMarix(rows, columns);
+PrintArray(matrix);
+System.Console.WriteLine();
+int num1 = Prompt("Введите индекс строки: ");
+int num2 = Prompt("Введите индекс столбца: ");
+System.Console.WriteLine(FindIndex(matrix, num1, num2));
