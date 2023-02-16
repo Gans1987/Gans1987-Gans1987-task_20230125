cec751b647c1043b2b973e3d8d0163989836c888
Mihail <tamsjam@mail.ru>
2023-02-16 16:47:27 +0300

    Домашняя работа

---
+++ HomeWork_1/HomeWerk_27/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+
+static int F(int m, int n)
+{
+    if (m <= n)
+    {
+        return m + F(m + 1, n);
+    }
+    return 0;
+}
+
+int m = 4;
+int n = 8;
+Console.WriteLine(F(m, n));
+Console.ReadKey();
